f8af28205ad73616820ec071f3dc18f6fe1a4a5b
jzjz <jinquan10@gmail.com>
2018-04-28 19:49:26 -0700

    recycler view

--- app/src/main/java/org/parkpickup/MainActivity.java
+++ app/src/main/java/org/parkpickup/MainActivity.java
@@ -2,11 +2,32 @@ package org.parkpickup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.parkpickup.api.ParkPickupContorllerV1;
+import org.parkpickup.client.ClientEnv;
+import org.parkpickup.client.ParkPickupV1Client;
 
 public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ParkPickupContorllerV1 parkPickupContorllerV1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.parkPickupContorllerV1 = new ParkPickupV1Client(ClientEnv.TEST);
+        this.parkPickupContorllerV1.getParks()
+
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new ParksRecyclerAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
